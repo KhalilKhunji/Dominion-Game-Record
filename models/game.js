4e86009ad0dbe69e6d5f88744ed53ecc3261a371
KhalilKhunji <khalilkhunji@gmail.com>
2024-07-28 14:53:46 +0300

    Added validation to Kingdom and Player schemas

--- models/game.js
+++ models/game.js
@@ -1,5 +1,21 @@
 const mongoose = require("mongoose");
 
+const validateKingdom = () => {
+    if (array.length === 10) {
+        return true;
+    } else {
+        return false;
+    };
+};
+
+const validatePlayers = () => {
+    if (array.length >= 2 && array.length <= 6) {
+        return true;
+    } else {
+        return false;
+    };
+};
+
 const playerSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -9,14 +25,20 @@ const playerSchema = new mongoose.Schema({
 });
 
 const kingdomSchema = new mongoose.Schema({
-    cards: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Card'
-    }]
+    cards: {
+        type: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Card'
+            }],
+        validate: [validateKingdom, 'Kingdoms need to be 10']
+    }
 });
 
 const gameSchema = new mongoose.Schema({
-    players: [playerSchema],
+    players: {
+        type: [playerSchema],
+        validate: [validatePlayers, 'Players need to be 2-6.']
+    },
     enjoyability: {
         type: String,
         enum: ['1','2','3','4','5','6','7','8','9','10']
